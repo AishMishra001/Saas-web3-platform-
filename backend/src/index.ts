c43a269bc85a45ee665687e1791df757f753595f
AishMishra001 <aishlunatic001@gmail.com>
2024-11-19 22:16:02 +0530

    creating backend schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,7 @@
 const express = require("express") ; 
 
 const app = express() ; 
+
+// postgres + prisma 
+
+// postgresql://neondb_owner:1v8nMAQhVskC@ep-noisy-brook-a5kgrs8u.us-east-2.aws.neon.tech/neondb?sslmode=require
