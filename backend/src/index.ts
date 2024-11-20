4dd97574ed743812a8142914b4593684472ca53b
AishMishra001 <aishlunatic001@gmail.com>
2024-11-20 21:20:13 +0530

    postgres schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,4 @@ const express = require("express") ;
 
 const app = express() ; 
 
-// postgres + prisma 
-
-// postgresql://neondb_owner:1v8nMAQhVskC@ep-noisy-brook-a5kgrs8u.us-east-2.aws.neon.tech/neondb?sslmode=require
+// postgres + prisma ( url in aws.txt locally ) 
